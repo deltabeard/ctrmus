81d5ebb901284e80f42a9b89874c557ed5e719eb
Mahyar Koshkouei <deltabeard@users.noreply.github.com>
2017-02-04 14:58:02 +0000

    Revert "sorting works, free directory"
    This reverts commit 019a14fb44511b6dfcdb9e738fa80b594a4a331c.

--- source/main.c
+++ source/main.c
@@ -125,6 +125,7 @@ int main(int argc, char** argv)
 	Thread t2 = NULL;
 	if (!debug) t2 = threadCreate(f_player, NULL, STACKSIZE, 0x18, -2, true);
 
+/*
 	// WORKING VERSION
 	int nbDirs;
 	int nbFiles;
@@ -135,6 +136,25 @@ int main(int argc, char** argv)
 	// todo: put files and folders together
 	foldernames = files;
 	nbFolderNames = nbFiles;
+*/
+/*
+	// NON WORKING VERSION
+	int nbDirs, nbFiles;
+	char** dirs;
+	char** files;
+	obtainDir(&dirs, &files, &nbDirs, &nbFiles, SORT_NAME_AZ);
+	nbFolderNames = nbFiles;
+*/
+
+	const char* baseListName = "filepath/listname00";
+	listnames = (char**)malloc(nbListNames*sizeof(char*));
+	for (int i=0; i<nbListNames; i++) {
+		listnames[i] = (char*)malloc((strlen(baseListName)+1)*sizeof(char));
+		memcpy(listnames[i], baseListName, strlen(baseListName));
+		listnames[i][strlen(baseListName)-1] = '0' + (i%10);
+		listnames[i][strlen(baseListName)-2] = '0' + (i/10%10);
+		listnames[i][strlen(baseListName)-0] = 0;
+	}
 
 	touchPosition oldTouchPad;
 	touchPosition orgTouchPad;
@@ -298,9 +318,6 @@ static int obtainFoldersSizes(int *nbDirs, int *nbFiles) {
 			num_files++;
 		}
 	}
-	if(closedir(dp) != 0)
-	goto err;
-
 	ret = 0;
 	*nbDirs = num_dirs;
 	*nbFiles = num_files;
@@ -338,13 +355,10 @@ static int obtainFolders(char** dirs, char** files, enum sorting_algorithms sort
 		}
 	}
 
-	if(closedir(dp) != 0)
-		goto err;
-
 	if(sort == SORT_NAME_AZ)
 	{
-		qsort(dirs, num_dirs, sizeof(char*), sortName);
-		qsort(files, num_files, sizeof(char*), sortName);
+		qsort(&dirs, num_dirs, sizeof(char*), sortName);
+		qsort(&files, num_files, sizeof(char*), sortName);
 	}
 	ret = 0;
 
