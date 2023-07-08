2ff0853ad4a559284d0006ed0c900b0f55be3cf5
Mahyar Koshkouei <mk@deltabeard.com>
2023-07-08 23:17:34 +0100

    error: add instructions on dumping DSP firmware
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- source/error.c
+++ source/error.c
@@ -15,7 +15,10 @@ char* ctrmus_strerror(int err)
 	switch(err)
 	{
 		case NDSP_INIT_FAIL:
-			error = "NDSP Initialisation failed";
+			error = "NDSP Initialisation failed.\nYou may need to dump DSP firmware.\n"
+				"You can do this within Rosalina (using Luma CFW):\n"
+				" 1. Press L+Down+Select\n 2. Misc. Options.\n 3. Dump DSP Firmware.\n"
+				" 4. Restart ctrmus.";
 			break;
 
 		case DECODER_INIT_FAIL:
